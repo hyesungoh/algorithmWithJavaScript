de909709645d74b3f193d9fd32b594c4a498ba47
hyesung oh <haesungoh414@gmail.com>
2021-08-23 23:20:44 +0900

    프로그래머스 문자열 내림차순으로 배치하기

--- Problem/test.js
+++ Problem/test.js
@@ -1,5 +1,2 @@
-function solution(arr, divisor) {
-    const answer = arr.filter((n) => n % divisor === 0);
-    console.log(answer.length);
-    return answer.length > 0 ? answer.sort((a, b) => a - b) : [-1];
-}
+const solution = (s) => s.split("").sort().reverse().join("")
+
---
+++ Problem/프로그래머스_문자열내림차순으로배치하기.js
@@ -0,0 +1,1 @@
+const solution = (s) => s.split("").sort().reverse().join("");
