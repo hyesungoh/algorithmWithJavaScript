947ece9c362a426aa459fae3bdaf15b0018a866e
Hyesung Oh <haesungoh414@gmail.com>
2021-09-01 16:19:31 +0900

    프로그래머스 콜라츠 추측

--- Problem/프로그래머스_콜라츠_추측.js
+++ Problem/프로그래머스_콜라츠_추측.js
@@ -1,5 +1,5 @@
 const solution = (n, cnt = 0) => {
     if (cnt > 500) return -1;
     if (n === 1) return cnt;
-    solution(n % 2 === 0 ? n / 2 : n * 3 + 1, cnt + 1);
+    return solution(n % 2 === 0 ? n / 2 : n * 3 + 1, cnt + 1);
 };
