93944596c9e39cdae85d2a0320aad9a3921ba13e
Hyesung Oh <haesungoh414@gmail.com>
2021-09-06 23:56:35 +0900

    Update 프로그래머스 2016년

--- Problem/프로그래머스_2016년.js
+++ Problem/프로그래머스_2016년.js
@@ -1,7 +1,6 @@
 const solution = (m, d) => {
     const words = ["FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU"];
     const days = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-    const curDay = days.filter((_, idx) => idx < m).reduce((a, c) => a + c) + d;
-    const curDow = (curDay % words.length) - 1;
-    return curDow === -1 ? "THU" : words[curDow];
+    const curDay = days.filter((_, idx) => idx < m).reduce((a, c) => a + c) + d - 1;
+    return words[curDay % 7];
 };
