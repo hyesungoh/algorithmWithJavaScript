8cf11d5096f1da21c536efc3c96722cca1bae09c
Hyesung Oh <haesungoh414@gmail.com>
2021-11-10 15:50:43 +0900

    프로그래머스 가장 큰 수

---
+++ Problem/프로그래머스_가장_큰_수.js
@@ -0,0 +1,7 @@
+function solution(numbers) {
+    const answer = numbers
+        .map((n) => n.toString())
+        .sort((a, b) => b + a - (a + b))
+        .join("");
+    return answer[0] === "0" ? "0" : answer;
+}
